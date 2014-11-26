a9a53431ba9af42e92edc37e097c21a93d794a7d
Uvadzucumi <uvadzucumi@gmail.com>
2014-11-27 00:18:48 +0400

    * fix warnings

--- map/noise_mask.cpp
+++ map/noise_mask.cpp
@@ -14,8 +14,8 @@ void CNoiseMask::Generate(){
     int noise_seed=rand()%RAND_MAX;
 
     Perlin *noise=new Perlin(NOISE_OCTAVES, NOISE_FREQUENCY, NOISE_AMPLITUDE, noise_seed);
-    for(unsigned int y=0; y < m_height; y++){
-        for(unsigned int x=0; x < m_width; x++){
+    for(int y=0; y < m_height; y++){
+        for(int x=0; x < m_width; x++){
             float height=noise->Get((float)x/m_width, (float)y/m_height);
             int index=y*m_width+x;
             m_mask[index]=height;
@@ -26,8 +26,8 @@ void CNoiseMask::Generate(){
 
     if(min_height<0){
         float dh=min_height*-1;
-        for(unsigned int y=0; y < m_height; y++){
-            for(unsigned int x=0; x < m_width; x++){
+        for(int y=0; y < m_height; y++){
+            for(int x=0; x < m_width; x++){
                 int index=y*m_width+x;
                 m_mask[index]+=dh;
             }
